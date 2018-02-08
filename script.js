f20c838778f306d1c849b17e585c50e4d3e14470
cgroark <colingroark@gmail.com>
2018-02-07 22:07:14 -0800

    styling and fixing navbar so its fixed

--- script.js
+++ script.js
@@ -3,3 +3,15 @@
       $(".button-collapse").sideNav();
     });
 
+// $(document).ready(function(){
+//   $(window).scroll(function(){
+//   	var scroll = $(window).scrollTop();
+// 	  if (scroll > 700) {
+// 	    $(".nav-color").css("background-color" , "transparent");
+// 	  }
+
+// 	  else{
+// 		  $(".nav-color").css("background-color" , "#6AA9AD");  	
+// 	  }
+//   })
+// })
