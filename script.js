1e6f637a8e3c0b77d72d80d373ed55016c626b02
cgroark <colingroark@gmail.com>
2018-02-11 21:47:22 -0800

    internal links and navbar

--- script.js
+++ script.js
@@ -3,15 +3,36 @@
       $(".button-collapse").sideNav();
     });
 
-// $(document).ready(function(){
-//   $(window).scroll(function(){
-//   	var scroll = $(window).scrollTop();
-// 	  if (scroll > 700) {
-// 	    $(".nav-color").css("background-color" , "transparent");
-// 	  }
-
-// 	  else{
-// 		  $(".nav-color").css("background-color" , "#6AA9AD");  	
-// 	  }
-//   })
-// })
+$('a[href*="#"]')
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 500, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
