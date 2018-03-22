bbb89884dcc9b6af21d730d734aaab92ea95f3d3
cgroark <colingroark@gmail.com>
2018-03-22 13:53:21 -0700

    navbar transition

--- script.js
+++ script.js
@@ -7,36 +7,52 @@
       $(".dropdown-button").dropdown();
     });
 
+var parallaxbottom = $('.parallax-container').offset().top + $('.parallax-container').height() - 40;
+var sectionprojects = $('.projects').offset().top - 60;
+var sectionexp = $('.experience-image').offset().top -70;
+var contact = $('.contact-div').offset().top -90;
+$(window).on('scroll',function(){
+
+    var stop = Math.round($(window).scrollTop());
+ 
+    if (stop > parallaxbottom && stop < sectionprojects) {
+        $('.about-nav').addClass('color-shift');
+        $('.work-nav').removeClass('color-shift');
+    } else if(stop > sectionprojects && stop < sectionexp){
+        $('.about-nav').removeClass('color-shift');
+        $('.work-nav').addClass('color-shift');
+        $('.expe-nav').removeClass('color-shift');
+    } else if(stop > sectionexp && stop < contact){
+        $('.work-nav').removeClass('color-shift');
+        $('.expe-nav').addClass('color-shift');
+         $('.con-nav').removeClass('color-shift');
+    }else if(stop > contact){
+        $('.expe-nav').removeClass('color-shift');
+        $('.con-nav').addClass('color-shift');
+    }else{
+        $('.about-nav').removeClass('color-shift');
+        $('.work-nav').removeClass('color-shift');
+        $('.expe-nav').removeClass('color-shift');
+        $('.con-nav').removeClass('color-shift');
+        
+    }
+
+});
+
 $('a[href*="#"]')
   .not('[href="#"]')
   .not('[href="#0"]')
   .click(function(event) {
-    if (
-      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
-      && 
-      location.hostname == this.hostname
-    ) {
-      // Figure out element to scroll to
+    if ( location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
       var target = $(this.hash);
+
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      // Does a scroll target exist?
       if (target.length) {
-        // Only prevent default if animation is actually gonna happen
         event.preventDefault();
         $('html, body').animate({
           scrollTop: target.offset().top
-        }, 500, function() {
-          // Callback after animation
-          // Must change focus!
-          var $target = $(target);
-          $target.focus();
-          if ($target.is(":focus")) { // Checking if the target was focused
-            return false;
-          } else {
-            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-            $target.focus(); // Set focus again
-          };
-        });
+        }, 800);
+        return false;
       }
     }
   });
